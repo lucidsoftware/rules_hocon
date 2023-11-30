d0956c23f7f40a6fc48e752919caa163861d8f37
Thayne McCombs <thayne@lucidchart.com>
2023-11-30 01:12:57 -0700

    Remove formating from bazel build
    because apparantly it causes problems when used. Instead call executable directly from the format.sh
    script

--- scripts/format.sh
+++ scripts/format.sh
@@ -10,10 +10,33 @@
 set -o pipefail
 cd "$(dirname "$0")/.."
 
+bazel build @rules_scala_annex//rules/scalafmt
+scalafmtbin="bazel-bin/external/rules_scala_annex/rules/scalafmt/scalafmt-bin"
+
+_scalafmt() {
+  find hocon-compiler -name '*.scala' -exec $scalafmtbin --config "$PWD/.scalafmt.conf" "$PWD/{}" "$PWD/{}" \;
+}
+
+_scalafmt-check() {
+  tfile="$(mktemp)"
+  failed=0
+  while IFS= read -r -d $'\0' f; do
+    $scalafmtbin --config "$PWD/.scalafmt.conf" "$PWD/$f" "$tfile"
+    if ! diff -u "$f" "$tfile"; then
+      (( failed++ )) || true
+    fi
+  done < <(find hocon-compiler -name '*.scala' -print0)
+
+  if (( failed > 0 )); then
+    printf "ERROR: %d files were not correctly formatted. Please run scripts/format.sh" $failed >&2
+    exit 1
+  fi
+}
+
 if [ "$1" != check ]; then
     bazel run buildifier
-    bazel run :scalafmt
+    _scalafmt
 else
     bazel run buildifier_check
-    bazel run :scalafmt-test
+    _scalafmt-check
 fi
